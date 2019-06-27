6c73fd86298540dd9432eb91d353369500cc1d98
Eetu Lauren <30345683+Virus629@users.noreply.github.com>
2019-06-27 21:06:31 +0300

    Add files via upload

--- Server/main.lua
+++ Server/main.lua
@@ -7,6 +7,29 @@ if settings.useDebug then
 end
 
 --Save user info :3
+local dataSaved = false
+
+RegisterNetEvent('frbs:saveUser')
+AddEventHandler('frbs:saveUser', function(source)
+    local src = source
+    local steamID = GetPlayerIdentifiers(src)[1] --Taken from esx
+    local license = GetPlayerIdentifiers(src)[2] 
+    local playerName = GetPlayerName(src)
+
+    if dataSaved == false then
+        MySQL.Async.execute('INSERT INTO users (`id`, `license`, `username`) VALUES (@id, @license, @username)',
+        {
+            ['@id'] = steamID,
+            ['@license'] = license,
+            ['@username'] = playerName
+        }, function(rowsChanged)
+            FRBS.Debug(rowsChanged)
+            dataSaved = true
+        end)
+    else
+        FRBS.Debug('Already done')
+    end
+end)
 
 --Voidaan käyttää pelaajan leftatessa johonkin esim. datan tallentamiseen
 AddEventHandler('playerDropped', function(reason)
@@ -15,6 +38,7 @@ AddEventHandler('playerDropped', function(reason)
 
     --print("Player " .. id .. " Has been " .. reason)
     FRBS.Debug('Player '.. id .. ' has ' .. reason)
+    TriggerEvent('frbs:saveUser', source)
 end)
 
 --Voidaan käyttää pelaajan lataamiseen yms.
@@ -24,20 +48,4 @@ AddEventHandler('playerConnecting', function(playerName, setKickReason)
 
     FRBS.Debug('Player '.. playerName .. ' is connecting')
     FRBS.Debug('IP is: '.. ip)
-end)
-
-RegisterCommand('save', function(source)
-    local src = source
-    local steamID = GetPlayerIdentifiers(src)[1] --Taken from esx
-    local license = GetPlayerIdentifiers(src)[2] 
-    local playerName = GetPlayerName(src)
-
-    MySQL.Async.execute('INSERT INTO users (`id`, `license`, `username`) VALUES (@id, @license, @username)',
-    {
-        ['@id'] = steamID,
-        ['@license'] = license,
-        ['@username'] = playerName
-    }, function(rowsChanged)
-        FRBS.Debug(rowsChanged)
-    end)
 end)
