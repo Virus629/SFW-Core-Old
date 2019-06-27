e5f6a8592e753ede1e8bc05fcd4a9eb697e2f574
Eetu Lauren <30345683+Virus629@users.noreply.github.com>
2019-06-27 20:29:58 +0300

    Dataa menee nyt databaseen

--- IMPORT_ME.sql
+++ IMPORT_ME.sql
@@ -2,8 +2,9 @@ CREATE DATABASE IF NOT EXISTS `base`;
 USE `base`;
 
 CREATE TABLE IF NOT EXISTS `users` (
-    `id` varchar(20) NULL,
+    `id` varchar(50) NULL,
     `license` varchar(50) NULL,
     `username` varchar(50) NULL,
+
     PRIMARY KEY (`id`)
-); #Creates users table where we store user information
+);
