db12fbabb9996a56d2cf67f35483de0dbaa14ef5
Eetu Lauren <30345683+Virus629@users.noreply.github.com>
2019-07-18 17:37:32 +0300

    Add files via upload

--- DUMP_ME.sql
+++ DUMP_ME.sql
@@ -1,11 +1,14 @@
 CREATE DATABASE IF NOT EXISTS `base`; 
 USE `base`;
 
-CREATE TABLE IF NOT EXISTS `users` (
-    `id` varchar(50) NULL,
-    `license` varchar(50) NULL,
-    `username` varchar(50) NULL,
-    `position` varchar(50) NULL,
-
-    PRIMARY KEY (`id`)
-);
+CREATE TABLE `users` (
+	`id` VARCHAR(50) NOT NULL,
+	`license` VARCHAR(50) NULL DEFAULT NULL,
+	`username` VARCHAR(50) NULL DEFAULT NULL,
+	`lastPosition` VARCHAR(255) NULL DEFAULT NULL,
+	`rank` VARCHAR(25) NULL DEFAULT 'user',
+	PRIMARY KEY (`id`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
